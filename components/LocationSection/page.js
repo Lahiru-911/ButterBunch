4f5b088ed750ec6a1ac662ecfc3302ffa875814e
Lahiru Kavinda <lahiruxk@gmail.com>
2025-03-22 17:53:30 +0530

    update

--- components/LocationSection/page.js
+++ components/LocationSection/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function LocationSection() {
   return (
-    <section className="relative text-white text-center py-24 px-5 overflow-hidden h-[600px] md:h-[700px]">
+    <section className="relative text-white text-center py-24 px-5 overflow-hidden h-[400px] md:h-[500px]">
       {/* Background Image */}
       <div
         className="absolute inset-0 z-0 bg-cover bg-center"
@@ -11,22 +11,22 @@ export default function LocationSection() {
           backgroundAttachment: "fixed",
         }}
       >
-        <div className="absolute inset-0 bg-black/50"></div>
+        <div className="absolute inset-0 bg-black/20"></div>
       </div>
 
       {/* Content */}
       <div className="relative z-10 max-w-[800px] mx-auto flex flex-col justify-center items-center h-full">
-        <h2 className="text-3xl md:text-4xl font-bold mb-4">
+        <h2 className="text-2xl md:text-4xl font-bold mb-4">
           LET'S CELEBRATE!
         </h2>
-        <p className="text-xl md:text-2xl font-semibold mb-6">
+        <p className="text-base md:text-xl font-normal mb-6">
           A cake makes every celebration sweeter. Whether you're planning a
           party or surprising someone with a treat, our custom cakes delivered
           straight to your door are the perfect way to add a little extra joy to
           every occasion.
         </p>
         <Link href="/contact">
-          <button className="bg-[#ff532c] text-white px-5 py-2 rounded-4xl hover:bg-[#ff412cf4] transition duration-300">
+          <button className="bg-[#ff532c] text-white text-sm md:px-5 md:py-2 px-3 py-1 rounded-4xl hover:bg-[#ff412cf4] transition duration-300">
             Order Now
           </button>
         </Link>
